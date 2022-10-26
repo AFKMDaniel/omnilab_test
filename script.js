dc969a422a0a162c9906386de5324e785f3702f1
AFKMDaniel <danich734@gmail.com>
2022-10-26 19:34:30 +1000

    added video block

--- script.js
+++ script.js
@@ -18,5 +18,29 @@ const moveSlider = (button,oppositeButton,edgeSlide,step) => {
 }
 
 right.addEventListener('click',() => moveSlider(right, left, 4, 1))
-
 left.addEventListener('click', () => moveSlider(left, right, 0 ,-1))
+
+const animElements = document.querySelectorAll('.anim_element');
+
+function isVisible(elem) {
+    let coords = elem.getBoundingClientRect();
+    let windowHeight = document.documentElement.clientHeight;
+  
+    // верхний край элемента виден?
+    let topVisible = coords.top > 0 && coords.top < windowHeight / 1;
+  
+    // нижний край элемента виден?
+    let bottomVisible = coords.bottom < windowHeight / 1 && coords.bottom > 0;
+  
+    return topVisible || bottomVisible;
+  }
+
+window.addEventListener('scroll', () => {
+    animElements.forEach(animElement => {
+        if(isVisible(animElement)){
+            animElement.classList.add('active');
+        } else {
+            animElement.classList.remove('active');
+        }
+    })
+})
