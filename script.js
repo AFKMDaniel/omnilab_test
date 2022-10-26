e6c80e4d212be01d7402125731236747b496d918
AFKMDaniel <danich734@gmail.com>
2022-10-26 11:17:58 +1000

    added moveSlider function

--- script.js
+++ script.js
@@ -4,28 +4,19 @@ const slider = document.querySelector('#slider');
 const sliderContent = document.querySelector('#slider-content');
 let currentSlide = 2;
 
-right.addEventListener('click', () => {
-    currentSlide++;
-    if (left.classList.contains('close')){
-        left.removeAttribute('disabled');
-        left.classList.remove('close');
+const moveSlider = (button,oppositeButton,edgeSlide,step) => {
+    currentSlide += step;
+    if (oppositeButton.classList.contains('close')){
+        oppositeButton.removeAttribute('disabled');
+        oppositeButton.classList.remove('close');
     }
-    if (currentSlide === 4) {
-        right.setAttribute('disabled','true');
-        right.classList.add('close');
+    if (currentSlide === edgeSlide) {
+        button.setAttribute('disabled','true');
+        button.classList.add('close');
     }
     sliderContent.style.left= `-${100 * currentSlide}%`;
-})
+}
 
-left.addEventListener('click', () => {
-    currentSlide--;
-    if (right.classList.contains('close')){
-        right.removeAttribute('disabled');
-        right.classList.remove('close');
-    }
-    if (currentSlide === 0){
-        left.setAttribute('disabled','true');
-        left.classList.add('close');
-    }
-    sliderContent.style.left= `-${100 * currentSlide}%`;
-})
+right.addEventListener('click',() => moveSlider(right, left, 4, 1))
+
+left.addEventListener('click', () => moveSlider(left, right, 0 ,-1))
