84c37473208468798fc1fd2057c1f999ef92bb7a
AFKMDaniel <danich734@gmail.com>
2022-10-27 13:49:58 +1000

    i think i did the work

--- script.js
+++ script.js
@@ -27,10 +27,10 @@ function isVisible(elem) {
     let windowHeight = document.documentElement.clientHeight;
   
     // верхний край элемента виден?
-    let topVisible = coords.top > 0 && coords.top < windowHeight / 1.5;
+    let topVisible = coords.top > 0 && coords.top < windowHeight;
   
     // нижний край элемента виден?
-    let bottomVisible = coords.bottom < windowHeight / 1.5 && coords.bottom > 0;
+    let bottomVisible = coords.bottom < windowHeight && coords.bottom > 0;
   
     return topVisible || bottomVisible;
   }
